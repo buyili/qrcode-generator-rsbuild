2afc91046403b62508eb757f030f05161154e228
buyili <1132989278@qq.com>
2025-08-01 22:07:16 +0800

    添加 zip 打包脚本

---
+++ utils/zip.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+import archiver from 'archiver';
+const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+
+const folderName = path.join('zip')
+try {
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName);
+  }
+
+  const output = fs.createWriteStream(path.join(folderName, 'qrcode-generator-v' + packageJson.version + '.zip'));
+  
+  const archive = archiver('zip');
+
+  archive.on('error', function (err) {
+    throw err;
+  });
+
+  archive.pipe(output);
+
+  // 压缩文件夹
+  archive.directory('dist/', false);
+
+  // 完成压缩
+  archive.finalize();
+
+} catch (err) {
+  console.error(err);
+}
